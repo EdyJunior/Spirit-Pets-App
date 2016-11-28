0b16cf5a92b910b7f02bc159621c489199816d70
Edvaldo Junior <dujunior1300@gmail.com>
2016-11-28 10:10:01 -0300

    Add empty project

---
+++ SpiritPets/SpiritPets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpiritPets
+//
+//  Created by Edvaldo Junior on 28/11/16.
+//  Copyright © 2016 Edvaldo Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
