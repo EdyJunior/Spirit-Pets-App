d7e2a83a8a6e4f515572904633c80718999efacc
padrao <padrao@macbook-pro-de-berg.local>
2016-11-29 15:39:24 -0200

    added auto layout to pet select screen. on mode portrait.

---
+++ SpiritPets/SpiritPets/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  SpiritPets
+//
+//  Created by padrao on 29/11/16.
+//  Copyright © 2016 Edvaldo Junior. All rights reserved.
+//
+
+import Foundation
+
+struct Pet{
+
+
+}
--- SpiritPets/SpiritPets/PetSelectionViewController.swift
+++ SpiritPets/SpiritPets/PetSelectionViewController.swift
@@ -11,24 +11,28 @@ import UIKit
 class PetSelectionViewController: UIViewController {
 
     
-    let pickerView = AKPickerView(frame: CGRect(origin: CGPoint.init(x: 50, y: 50), size: CGSize(width:200, height: 200)))
+    let pickerView = AKPickerView(frame: CGRect(origin: CGPoint.init(x: 0, y: 20), size: CGSize(width:200, height: 200)))
     
     @IBOutlet var statusLabels: [UILabel]!
     @IBOutlet weak var hpLabel: UILabel!
     @IBOutlet weak var atkLabel: UILabel!
     @IBOutlet weak var dfsLabel: UILabel!
     
-    let images = [UIImage.init(named: "pet0"), UIImage.init(named: "pet1")]
-    //let images = ["ola", "povo"]
+    let images = [UIImage.init(named: "pet0"), UIImage.init(named: "pet1"),UIImage.init(named: "pet0"), UIImage.init(named: "pet1")]
     
-        
+    let pett0 = PetStatus(hp: 100, atk: 80, dfs: 60)
+    let pett1 = PetStatus(hp: 100, atk: 70, dfs: 70)
+    let pets = [ PetStatus(hp: 200, atk: 180, dfs: 60), PetStatus(hp: 150, atk: 70, dfs: 170),PetStatus(hp: 200, atk: 180, dfs: 60), PetStatus(hp: 150, atk: 70, dfs: 170)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.pickerView.frame.size.width = view.frame.width
+        self.pickerView.frame.size.height = view.frame.height / 3
         self.pickerView.delegate = self
         self.pickerView.dataSource = self
-        
+        self.pickerView.selectItem(0)
         
         self.view.addSubview(pickerView)
         self.pickerView.reloadData()
@@ -36,13 +40,31 @@ class PetSelectionViewController: UIViewController {
         self.setUpLabels()
     }
     
+    @IBAction func letsgoTap(_ sender: UIButton) {
+        //TODO: perform segue
+    }
+    //call this to round the status labels.
     func setUpLabels(){
         for label in statusLabels{
             label.layer.masksToBounds = true
             label.layer.cornerRadius = 10
         }
-        hpLabel.frame.size.width = hpLabel.frame.width/2
     }
+    
+    //status must be a array with 3 values: hp, atk, dfs.
+    func updateStatusLabels(status:[Int]){
+        for i in 0...statusLabels.count - 1{
+            statusLabels[i].frame.size.width = CGFloat(status[i])
+        }
+    }
+    
+    //pog pra testar
+    struct PetStatus {
+        var hp:Int
+        var atk:Int
+        var dfs:Int
+    }
+    //***************
 }
 
 
@@ -53,9 +75,10 @@ extension PetSelectionViewController: AKPickerViewDelegate{
         return images[item]!
     }
     
-    //func pickerView(_ pickerView: AKPickerView, titleForItem item: Int) -> String {
-      //  return images[item]
-    //}
+    func pickerView(_ pickerView: AKPickerView, didSelectItem item: Int) {
+        print("selecionou \(images[item])")
+        self.updateStatusLabels(status: [pets[item].atk, pets[item].dfs, pets[item].dfs])
+    }
 }
 
 extension PetSelectionViewController: AKPickerViewDataSource{
