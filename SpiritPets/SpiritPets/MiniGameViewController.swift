14dd9e59e6e584cfd12764e43a7963a2ca57c943
Augusto Falcao <falcaoaugustos@gmail.com>
2016-11-29 10:50:01 -0300

    Create view controller to play the mini-games

--- SpiritPets/SpiritPets/MiniGameViewController.swift
+++ SpiritPets/SpiritPets/MiniGameViewController.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2016 Edvaldo Junior. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+class MiniGameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let scene = MiniGame01Scene(size: UIScreen.main.bounds.size)
+        
+        // Set the scale mode to scale to fit the window
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene
+        if let view = self.view as! SKView? {
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
