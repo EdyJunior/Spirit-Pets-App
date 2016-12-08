f5c40a7710cbf3b32d933e5be41b984989a02ccf
Augusto Falcao <falcaoaugustos@gmail.com>
2016-12-08 10:04:06 -0300

    Including watch assets

---
+++ SpiritPets/SpiritPets WatchKit Extension/PetAnimationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PetAnimationViewController.swift
+//  SpiritPets
+//
+//  Created by Augusto Falcão on 12/7/16.
+//  Copyright © 2016 Edvaldo Junior. All rights reserved.
+//
+
+import Foundation
