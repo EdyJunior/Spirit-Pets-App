5634d6fb5a51da2da8a7d04979206f7e40504410
bergmpe <bergmpe@gmail.com>
2017-08-09 09:40:48 -0300

    it created FirebaseHelper class and added firebase pods to login with google accounts.

---
+++ SpiritPets/SpiritPets/FirebaseHelper.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseHelper.swift
+//  SpiritPets
+//
+//  Created by padrao on 09/08/17.
+//  Copyright © 2017 Edvaldo Junior. All rights reserved.
+//
+
+import Firebase
+
+class FirebaseHelper {
+
+}
